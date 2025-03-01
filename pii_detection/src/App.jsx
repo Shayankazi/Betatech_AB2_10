b9fd092ae45780698cf2b3e2acdb0a8b085e1cd4
Shayan Kazi <shayakazi147@gmail.com>
2025-03-01 15:57:21 +0530

    ALl pages

--- pii_detection/src/App.jsx
+++ pii_detection/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
-import Home from './Component/home_page/home'
+import Home from "./Component/Home_page/home";
+import Aboutus from "./Component/About_us/aboutus";
+import Contactus from "./Component/Contact_us/contact";
+import DashboardUser from "./Component/Dashboard/dashUser";
+import DashboardAdmin from "./Component/Dashboard/dashAdmin";
+import LoginUser from "./Component/login/loginUser";
+import LoginAdmin from "./Component/login/loginAdmin"
 
 function App() {
-  
   return (
-    <Home></Home>
-  )
+    <>
+      <Home />
+      <Aboutus />
+      <Contactus />
+      <DashboardUser />
+      <DashboardAdmin />
+      <LoginUser />
+    </>
+  );
 }
 
-export default App
+export default App;
